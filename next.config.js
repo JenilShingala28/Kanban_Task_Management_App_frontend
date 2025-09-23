5b76cbd787b48a475fcf5fab79bc0fdd22003963
jenil shingala <jenilshingala28@gmail.com>
2025-09-24 00:03:58 +0530

    update config ts to js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { appDir: true },
+};
+
+module.exports = nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
